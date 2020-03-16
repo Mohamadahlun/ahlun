1d529720530f6ff38af2dc3cc60d4e332964349d
Mohamadahlun <62237542+Mohamadahlun@users.noreply.github.com>
2020-03-16 20:51:14 +0700

    Add files via upload

---
+++ monitor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSuperComputer
+{
+    class monitor
+    {
+        {
+            //resolution, supporthdmi, size
+            private String resolution;
+            private Boolean supportHdmi;
+            private double size;
+
+            public monitor(String resolution, Boolean supportHdmi, double size)
+            {
+                this.resolution = resolution;
+                this.supportHdmi = supportHdmi;
+                this.size = size;
+            }
+
+            private String getResolution()
+            {
+                return this.resolution;
+            }
+
+            private Boolean isSupportHdmi()
+            {
+                return this.supportHdmi;
+            }
+
+            private double getSize()
+            {
+                return this.size;
+            }
+
+            public override string ToString()
+            {
+                return $"Monitor resolution: {resolution} ; Monitor support hdmi : {supportHdmi}; Monitor dimensi : {size} inch ";
+            }
+        }
+    }
+
+}
+}
